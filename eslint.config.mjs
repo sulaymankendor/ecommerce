8814a8064c5c4ca8967b8c7f0040d96304bcc967
sulaymankendor <sulaymankendor18@gmail.com>
2025-02-23 15:10:14 +0000

    solving deployment issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,26 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+    },
+    rules: {
+      "@typescript-eslint/ban-ts-comment": [
+        "error",
+        {
+          "ts-ignore": "allow-with-description", // Allow @ts-ignore with a description
+          "ts-expect-error": "allow-with-description", // Allow @ts-expect-error with a description
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "off", // Disable no-explicit-any rule
+      "react-hooks/exhaustive-deps": "warn", // Show warnings for missing dependencies in useEffect
+    },
+  },
 ];
 
 export default eslintConfig;
