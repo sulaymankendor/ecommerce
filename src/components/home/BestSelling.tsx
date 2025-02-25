617ceae56d424c50fbf17e5da7465bbcc01c3004
sulaymankendor <sulaymankendor18@gmail.com>
2025-02-25 13:44:03 +0000

    added more margin top to the best selling products label

--- src/components/home/BestSelling.tsx
+++ src/components/home/BestSelling.tsx
@@ -8,7 +8,9 @@ function BestSelling() {
   const { products, isLoading } = useBestSellingProducts();
   return (
     <div className="mb-3">
-      <h1 className="font-bold text-3xl my-9 ml-8">Best Selling Products</h1>
+      <h1 className="font-bold text-3xl mb-9 mt-16 ml-8">
+        Best Selling Products
+      </h1>
 
       {isLoading ? (
         <div className="py-[6%]">
